819c1721f1f4347f457223bf95d60572e7d71de1
Samyak07-sd <114360899+Samyak07-sd@users.noreply.github.com>
2023-12-11 09:49:57 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+
+const API_KEY = 'API_KEY';
+const VIDEO_ID = 'UNLISTED_VIDEO_ID';
+
+let player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '360',
+        width: '640',
+        videoId: VIDEO_ID,
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+function onPlayerStateChange(event) {
+}
